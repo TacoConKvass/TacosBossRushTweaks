c7f7ba136dc4fcb9065ee875f23b5f3b3c4c01ab
TacoConKvass <e_frun@o2.pl>
2025-01-30 13:46:34 +0100

    [Fix] Reactive DR calculation

--- ReactiveDR.cs
+++ ReactiveDR.cs
@@ -7,31 +7,33 @@ using CalamityMod;
 
 namespace TacosBossRushTweaks;
 
-public class ReactiveDR : GlobalNPC
-{
+public class ReactiveDR : GlobalNPC {
 	public override bool InstancePerEntity => true;
 
 	public bool SpawnedInBossRush;
-	int Timer;
+	int timer;
 
 	public override void OnSpawn(NPC npc, IEntitySource source) {
 		SpawnedInBossRush = BossRushEvent.BossRushActive && CalamityUtils.IsABoss(npc);
-		Timer = 0;
+		timer = 0;
 	}
 
-	public override void AI(NPC npc) => Timer++;
+	public override bool PreAI(NPC npc) {
+		timer++;
+		return true;
+	}
 
 	public override void ModifyIncomingHit(NPC npc, ref NPC.HitModifiers modifiers) {
 		if (!SpawnedInBossRush) return;
 
 		float healthRatio = npc.life / (float)npc.lifeMax;
-		float timeRatio = Timer / CalamityMod.CalamityMod.bossKillTimes[npc.type];
+		float timeRatio = timer / (float)CalamityMod.CalamityMod.bossKillTimes[npc.type];
 
 		if (healthRatio + timeRatio >= 1) return;
 
 		float baseDR = npc.Calamity().DR;
 
 		float reactiveDR = ((1 - baseDR) * 10) - (((1 - baseDR) * 10) / (2 - timeRatio - healthRatio));
-		modifiers.FinalDamage *= Math.Clamp(1 / reactiveDR, 0, 1); 
+		modifiers.FinalDamage *= 1 - reactiveDR; 
 	}
 }
